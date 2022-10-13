d40830b7bfec2db882467ad88a73e651ad5e13e2
David M <PieKing1215@users.noreply.github.com>
2022-10-13 15:09:03 -0400

    About page setup

--- src/templates/about.rs
+++ src/templates/about.rs
@@ -1,9 +1,13 @@
 use perseus::Template;
 use sycamore::prelude::{view, Html, Scope, SsrNode, View};
 
+use crate::templates::{header::Header, navbar::Navbar};
+
 #[perseus::template_rx]
 pub fn about_page<G: Html>(cx: Scope) -> View<G> {
     view! { cx,
+        Header()
+        Navbar()
         p { "About." }
     }
 }
@@ -11,7 +15,10 @@ pub fn about_page<G: Html>(cx: Scope) -> View<G> {
 #[perseus::head]
 pub fn head(cx: Scope) -> View<SsrNode> {
     view! { cx,
-        title { "About Page | Perseus Example – Basic" }
+        meta(charset = "UTF-8")
+        meta(name = "viewport", content = "width=device-width, initial-scale=1.0")
+        title { "David Mahany | About" }
+        link(rel = "stylesheet", href = ".perseus/static/css/about.css")
     }
 }
 
